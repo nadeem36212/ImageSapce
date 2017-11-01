2e19669ac5a04a6016b4a313b5e8aa0a43685134
nadeem352 <n.sajid362@gmail.com>
2017-11-01 11:56:11 +0500

    ImageSpaceThirfGit

--- ImageSpace/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-} from 'react-native';
-import ImageSpace from './app/App';
-
-AppRegistry.registerComponent('ImageSpace', () => ImageSpace);
--- app/App.js
+++ app/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
  import GreyScreen from './Components/GreyScreen';
-import ScarletScreen from './Components/ScarletScreen';
-
+import CameraScreen from './Components/CameraScreen';
+import HomeScreen from './Components/HomeScreen';
 export default class ImageSpace extends Component {
     render() {
         return (
@@ -11,23 +11,30 @@ export default class ImageSpace extends Component {
                         key="root"
                         hideNavBar
                     >
-                        <Scene
-                            key="ss"
-                            component={ScarletScreen}
-                            title=""
-                            direction="horizontal"
-                            initial
-                            duration={0}
-                        />
+                    <Scene
+                        key="HomeScreen"
+                        component={HomeScreen}
+                        title=""
+                        direction="horizontal"
+                        //initial
+                        duration={0}
+                    />
+                    <Scene
+                        key="CameraScreen"
+                        component={CameraScreen}
+                        title=""
+                        direction="horizontal"
+                        initial
+                        duration={0}
+                    />
+                    <Scene
+                        key="gs"
+                        component={GreyScreen}
+                        title=""
+                        direction="horizontal"
 
-                        <Scene
-                            key="gs"
-                            component={GreyScreen}
-                            title=""
-                            direction="horizontal"
-                            initial
-                            duration={0}
-                        />
+                        duration={0}
+                    />
                     </Scene>
                 </Router>
         );
